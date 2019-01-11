604a05dbce87ca29fe05d0c9a6c7e5da1ca25ce5
v-wangxiaofeng <>
2019-01-11 11:24:41 +0800

    第二个分支增加了内容。

--- src/main/java/com/jd/Demo.java
+++ src/main/java/com/jd/Demo.java
@@ -4,5 +4,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("第一次创建分支提交");
         System.out.println("第二次提交");
+        //第三个分支创建并提交，第二个分支也提交
+        System.out.println("第三个分支创建并提交，第二个分支也提交");
     }
 }
