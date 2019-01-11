50326b2ce0fa4ebb7ec254a739872a100598c077
v-wangxiaofeng <>
2019-01-11 11:34:38 +0800

    从mastre和fenzhier合并之后提交

--- src/main/java/com/jd/Demo.java
+++ src/main/java/com/jd/Demo.java
@@ -6,5 +6,6 @@ public class Demo {
         System.out.println("第二次提交");
         //第三个分支创建并提交，第二个分支也提交
         System.out.println("第三个分支创建并提交，第二个分支也提交");
+        System.out.println("从mastre和fenzhier合并之后提交");
     }
 }
